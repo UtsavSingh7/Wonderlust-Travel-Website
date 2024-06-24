6a9936791bba088a968f38c76dc2b47f3262b3e0
UtsavSingh7 <utsavdev2826@gmail.com>
2024-06-24 18:21:13 +0530

    Connect Mongodb database

--- js/script.js
+++ js/script.js
@@ -7,9 +7,11 @@
     // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
 
-      
+
       form.addEventListener('submit', event => {
         if (!form.checkValidity()) {
+
+          
           event.preventDefault()
           event.stopPropagation()
         }
